9e2f90314b5f0a0ecc19378dbc2651a44e995788
PETER <PETER@DESKTOP-T7KDQA2>
2023-08-27 19:41:20 +0200

    wip

--- src/Pafetser.Yarf/Results.cs
+++ src/Pafetser.Yarf/Results.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Pafetser.Yarf;
 
-public interface IResults<out T>: IResult<IEnumerable<T>>, IEnumerable<T>
+public interface IResults<out T>: IResult<IEnumerable<T?>>, IEnumerable<T?>
 {
 
 }
-public record Results<T> : Result<IEnumerable<T>>, IResults<T>
+public record Results<T> : Result<IEnumerable<T?>>, IResults<T?>
 {
-    public IEnumerator<T> GetEnumerator()
+    public Results() { }
+    [SetsRequiredMembers]
+    public Results(IEnumerable<T?> value):base(value) { }
+    [SetsRequiredMembers]
+    public Results(params T?[] value) : this((IEnumerable<T?>)value) { }
+
+    public IEnumerator<T?> GetEnumerator()
     {
-        return (Value ?? Enumerable.Empty<T>()).GetEnumerator();
+        return (Value ?? Enumerable.Empty<T?>()).GetEnumerator();
     }
 
     IEnumerator IEnumerable.GetEnumerator()
