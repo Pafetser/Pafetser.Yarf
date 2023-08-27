82f4b974c8a596838865e36fd0f9617b8bd29011
PETER <PETER@DESKTOP-T7KDQA2>
2023-08-27 19:34:08 +0200

    wip

--- src/Pafetser.Yarf/ToExtensions.cs
+++ src/Pafetser.Yarf/ToExtensions.cs
@@ -2,14 +2,11 @@
 
 public static class ToExtensions
 {
-    public static TTarget To<TSource, TTarget>(TSource from)
+    public static TTarget To<TSource, TTarget>(this TSource from)
         where TSource : Result
         where TTarget : Result, new()
     {
         if (from is null) throw new ArgumentNullException(nameof(from));
-        var target = new TTarget();
-        foreach (var message in from.Messages)
-            target.WithMessage(message with { });
-        return target;
+        return new TTarget().WithMessages(from.Messages.Select(m => m with { }));
     }
 }
